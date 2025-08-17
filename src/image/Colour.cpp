2a84feafd6a9d8090b4781185f6476a2897cff39
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2025-08-17 03:17:11 +0100

    Initialise Colour using sRGB

--- src/image/Colour.cpp
+++ src/image/Colour.cpp
@@ -1,7 +1,31 @@
 #include "Colour.h"
 
 Colour::Colour() {
+	m_srgb.r = 0;
+	m_srgb.g = 0;
+	m_srgb.b = 0;
+	m_alpha = 1.;
+}
+
+Colour::Colour(const Colour& other) {
+	m_srgb = other.m_srgb;
 }
 
 Colour::~Colour() {
+}
+
+Colour& Colour::operator=(const Colour& other) {
+	if (this == &other) return *this;
+	m_srgb = other.m_srgb;
+	return *this;
+}
+
+Colour Colour::FromsRGB(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a) {
+	Colour out;
+
+	out.m_srgb.r = r;
+	out.m_srgb.g = g;
+	out.m_srgb.b = b;
+	out.m_alpha = (double)a / 255.;
+	return out;
 }
--- src/image/Colour.h
+++ src/image/Colour.h
@@ -3,9 +3,29 @@
 class Colour {
 public:
 	Colour();
+	Colour(const Colour& other);
 	~Colour();
 
+	Colour& operator=(const Colour& other);
+
+	/// <summary>
+	/// Assign Colour based on sRGB values
+	/// </summary>
+	/// <param name="r">0 to 255</param>
+	/// <param name="g">0 to 255</param>
+	/// <param name="b">0 to 255</param>
+	/// <param name="a">0 to 255</param>
+	/// <returns></returns>
+	static Colour FromsRGB(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255);
+
 private:
-	uint8_t m_r, m_g, m_b;
+	struct sRGB {
+		uint8_t r;
+		uint8_t g;
+		uint8_t b;
+	};
+	sRGB m_srgb;
+
+	double m_alpha;
 };
 
