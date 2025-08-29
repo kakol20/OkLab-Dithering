127501acb448808d85824f6ad7343db6d6ff618f
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2025-08-29 17:24:46 +0100

    Add blue noise matrix

--- src/image/Dither.cpp
+++ src/image/Dither.cpp
@@ -21,9 +21,29 @@ std::array<uint8_t, 256> Dither::m_bayer16{
 	 170, 106, 154,  90, 166, 102, 150,  86, 169, 105, 153,  89, 165, 101, 149,  85
 };
 
+std::array<uint8_t, 256> Dither::m_blueNoise16{
+	111,  53, 141, 160, 113, 194,  73, 176, 200,  53, 150,  94,  68,  42,  86, 252,
+	 31,  99, 238, 221,  37, 250, 147,  26,  42, 105, 219, 168, 193, 137,  21, 165,
+	124, 176,  80,  22,  67, 172, 122,  87, 211, 130, 247,  29, 115,  57, 228, 211,
+	 45, 200, 150, 130, 188, 104,  56, 235, 159,  64,   6, 179,  78, 241, 146,  69,
+		9, 244,  58,  91, 229,  13, 203,  33, 185, 101, 143, 205,  38,  92, 189, 106,
+	222, 162, 114,  40, 213, 155, 138,  72, 245,  85, 225,  51, 125, 156,  24, 134,
+	 84, 194,  27, 254,  77,  49, 177, 114,  20,  45, 167, 104, 253, 175, 210,  62,
+	100, 179, 143, 121, 171,  97, 235, 128, 214, 148, 198,  17,  73,  32, 238,  48,
+	231,  37,  70,  20, 204,  61,  25, 191,  88,  62, 111, 220, 139,  87, 119, 152,
+	207, 129, 243, 159, 223, 109,  39, 247, 163,  30, 233, 182,  55, 197, 169,  15,
+	107, 187,  54,  90, 136, 185, 152,  79,  51, 133,  98, 156,  40, 249,  95,  65,
+	 24,  76, 218,  43,   0,  68, 227, 120, 195, 239,  11,  75, 126,  27, 226, 142,
+	246, 173, 119, 199, 251, 103, 145,  21, 208, 172, 109, 217, 191,  83, 202, 162,
+	 34,  93, 149,  28, 165, 181,  58,  36,  90,  66,  46, 139, 166,  59, 116,  49,
+	215, 232,  63, 127,  82, 237, 216, 117, 158, 255, 184,  33, 242, 102,  12, 132,
+	 75, 190,  17, 209,  47,  96,  15, 135, 230,  81,  19, 123, 224, 206, 154, 181
+};
+
+bool Dither::m_mono = false;
 std::string Dither::m_distanceMode = "oklab";
 std::string Dither::m_mathMode = "srgb";
-bool Dither::m_mono = false;
+std::string Dither::m_matrixType = "bayer";
 
 void Dither::OrderedDither(Image& image, const Palette& palette) {
 	const int imgWidth = image.GetWidth();
@@ -44,8 +64,7 @@ void Dither::OrderedDither(Image& image, const Palette& palette) {
 				Colour::SetMathMode(Colour::MathMode::sRGB);
 			}
 
-			double threshold = (double)m_bayer16[MatrixIndex(x % 16, y % 16)];
-			threshold = ((threshold + 0.5) / 256.) - 0.5;
+			const double threshold = GetThreshold(x, y);
 
 			Colour threshold_c;
 			if (Colour::GetMathMode() == Colour::MathMode::sRGB) {
@@ -225,10 +244,11 @@ void Dither::NoDither(Image& image, const Palette& palette) {
 	}
 }
 
-void Dither::SetSettings(const std::string distanceType, const std::string mathMode, const bool mono) {
+void Dither::SetSettings(const std::string distanceType, const std::string mathMode, const bool mono, const std::string matrixType) {
 	m_distanceMode = distanceType;
 	m_mathMode = mathMode;
 	m_mono = mono;
+	m_matrixType = matrixType;
 }
 
 Colour Dither::ClosestColour(const Colour& col, const Palette& palette, const bool grayscale) {
@@ -285,10 +305,22 @@ Colour Dither::ClosestColour(const Colour& col, const Palette& palette, const bo
 	return closest;
 }
 
+double Dither::GetThreshold(const int x, const int y) {
+	double threshold = 0.;
+	if (m_matrixType == "bluenoise16") {
+		threshold = (double)m_blueNoise16[MatrixIndex(x % 16, y % 16, 16)];
+	} /*else if (m_matrixType == "bluenoise32") {
+		threshold = (double)m_blueNoise32[MatrixIndex(x % 32, y % 32, 32)];
+	}*/ else {
+		threshold = (double)m_bayer16[MatrixIndex(x % 16, y % 16, 16)];
+	}
+	return ((threshold + 0.5) / 256.) - 0.5;
+}
+
 Colour Dither::GetColourFromImage(const Image& image, const int x, const int y) {
 	const size_t index = image.GetIndex(x, y);
 	if (image.IsGrayscale()) {
-		const uint8_t data = image.GetData(index );
+		const uint8_t data = image.GetData(index);
 		return Colour::FromsRGB(data, data, data);
 	} else {
 		return Colour::FromsRGB(image.GetData(index + 0),
