b16a20a44fb72a5f38eef74fc49b32379017566d
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2026-03-10 05:23:06 +0000

    Prevent modulo bias

--- src/misc/Random.cpp
+++ src/misc/Random.cpp
@@ -6,11 +6,15 @@ unsigned int Random::Seed = 1;
 unsigned int Random::MaxRand = ~0;
 
 unsigned int Random::Rand() {
-	unsigned int state = Random::Seed * (unsigned int)747796405 + (unsigned int)2891336453;
-	unsigned int word = ((state >> ((state >> 28) + 4)) ^ state) * (unsigned int)27780373;
+	//unsigned int state = Random::Seed * (unsigned int)747796405 + (unsigned int)2891336453;
+	//unsigned int word = ((state >> ((state >> 28) + 4)) ^ state) * (unsigned int)27780373;
 
-	Random::Seed = (word >> 22) ^ word;
-	return Random::Seed;
+	//Random::Seed = (word >> 22) ^ word;
+	//return Random::Seed;
+	Random::Seed = Random::Seed * 747796405u + 2891336453u;
+	unsigned int state = Random::Seed;
+	unsigned int word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
+	return (word >> 22u) ^ word;
 }
 
 unsigned int Random::RandUInt(unsigned int min, unsigned int max) {
@@ -21,8 +25,14 @@ unsigned int Random::RandUInt(unsigned int min, unsigned int max) {
 		min = min ^ max;
 	}
 
-	const unsigned int delta = (max - min) + 1;
-	return (Random::Rand() % delta) + min;
+	const unsigned int range = (max - min) + 1;
+	const unsigned int threshold = Random::MaxRand - (Random::MaxRand % range);
+
+	unsigned int out = 0;
+	do {
+		out = Random::Rand();
+	} while (out > threshold);
+	return (out % range) + min;
 }
 
 double Random::RandDouble(double min, double max) {
@@ -31,7 +41,7 @@ double Random::RandDouble(double min, double max) {
 
 	const double range = max - min;
 
-	return ((double(rand) / double(Random::MaxRand)) * range) + min;
+	return ((double(rand) * (1. / double(Random::MaxRand))) * range) + min;
 }
 
 void Random::EpochSeed() {
